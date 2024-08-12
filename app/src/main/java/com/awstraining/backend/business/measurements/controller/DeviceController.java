e7ff45c95db37927d1f68a81dc2a26f7b983eb17
Filip Forszpaniak <filip.forszpaniak@capgemini.com>
2024-08-12 13:26:39 +0200

    added MeterRegistry

--- app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
+++ app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
@@ -9,6 +9,7 @@ import com.awstraining.backend.api.rest.v1.model.Measurement;
 import com.awstraining.backend.api.rest.v1.model.Measurements;
 import com.awstraining.backend.business.measurements.MeasurementDO;
 import com.awstraining.backend.business.measurements.MeasurementService;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,15 +23,19 @@ class DeviceController implements DeviceIdApi {
     private static final Logger LOGGER = LogManager.getLogger(DeviceController.class);
 
     private final MeasurementService service;
+    private final MeterRegistry registry;
 
     @Autowired
-    public DeviceController(final MeasurementService service) {
+    public DeviceController(final MeasurementService service, MeterRegistry registry
+        ) {
         this.service = service;
+        this.registry = registry;
     }
 
     @Override
     public ResponseEntity<Measurement> publishMeasurements(final String deviceId, final Measurement measurement) {
         LOGGER.info("Publishing measurement for device '{}'", deviceId);
+        registry.counter("publishMeasurements", "method", "invocation").increment();
         final MeasurementDO measurementDO = fromMeasurement(deviceId, measurement);
         service.saveMeasurement(measurementDO);
         return ResponseEntity.ok(measurement);
@@ -38,6 +43,7 @@ class DeviceController implements DeviceIdApi {
     @Override
     public ResponseEntity<Measurements> retrieveMeasurements(final String deviceId) {
         LOGGER.info("Retrieving all measurements for device '{}'", deviceId);
+        registry.counter("retrieveMeasurements", "method", "invocation").increment();
         final List<Measurement> measurements = service.getMeasurements()
                 .stream()
                 .map(this::toMeasurement)
